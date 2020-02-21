78b0423686374eb55f8fb0d3595e5ef8e5bf542f
Homegirl7417 <kj3477417@gmail.com>
2020-02-21 15:13:12 +0900

    PageTemplate, PostPage Update && EditorTemplate, EditorHeader Update

--- src/components/common/PageTemplate/PageTemplate.js
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
+import Header from 'components/common/Header';
+import Footer from 'components/common/Footer'
 
 //classNames 는 CSS 클래스를 조건부로 설정 할 때, 여러 클래스를 적용할 때 유용
 const cx = classNames.bind(styles);
 
-const PageTemplate = () => (
+const PageTemplate = ({children}) => (
     <div className={cx('page-template')}>
-        PageTemplate
+        <Header />
+        <main>
+            {children}
+        </main>
+        <Footer />
     </div>
 );
 
